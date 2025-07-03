f3b3862d476d91df9c49b4415e9c2b436e711fc7
Lucas Passarete <84470276+Passaret@users.noreply.github.com>
2025-07-03 11:07:03 +0000

    feat: joins

--- exercicios/3-consultando-dados-varias-tabelas/1-tipos-joins.sql
+++ exercicios/3-consultando-dados-varias-tabelas/1-tipos-joins.sql
@@ -1,1 +1,6 @@
 -- Crie uma consulta para realizar um left join com as tabelas "albums" e "artists"
+SELECT art.Name,
+alb.Title
+FROM albums as alb
+LEFT JOIN artists as art
+ON alb.ArtistId = art.ArtistId;
--- exercicios/3-consultando-dados-varias-tabelas/2-join-e-with.sql
+++ exercicios/3-consultando-dados-varias-tabelas/2-join-e-with.sql
@@ -1,4 +1,27 @@
 -- Crie uma consulta para realizar inner join com as tabelas "tracks", "albums" e "artists"
-
+SELECT 
+art.Name as artista,
+trk.Name as musica,
+alb.Title as album
+FROM tracks as trk
+INNER JOIN albums as alb
+ON trk.AlbumId = alb.AlbumId
+INNER JOIN artists as art
+ON art.ArtistId = alb.ArtistId
+;
 
 -- Refatore a consulta anterior usando a cláusula "with" e verifique o total de músicas existente na base de terminado artista, ex: Caetano Veloso
+WITH musicas as (
+SELECT 
+art.Name as artista,
+trk.Name as musica,
+alb.Title as album
+FROM tracks as trk
+INNER JOIN albums as alb
+ON trk.AlbumId = alb.AlbumId
+INNER JOIN artists as art
+ON art.ArtistId = alb.ArtistId)
+
+SELECT *
+FROM musicas
+WHERE artista like 'Caetano Veloso';
