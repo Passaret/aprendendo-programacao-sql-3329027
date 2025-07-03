736213e63d6fa2ee4f40c26cbecfe33bdc18fca9
Lucas Passarete <84470276+Passaret@users.noreply.github.com>
2025-07-03 11:41:48 +0000

    feat: melhorando consultas

--- exercicios/4-refinando-consulta/1-filtro.sql
+++ exercicios/4-refinando-consulta/1-filtro.sql
@@ -1,1 +1,14 @@
 -- Crie uma consulta para realizar inner join com as tabelas "tracks", "albums" e "artists". Além disso, apresente apenas as músicas do artista que contenha "Nação" no nome e título do álbum que não seja "Da Lama Ao Caos"
+SELECT
+art.Name as artista,
+trk.Name as musica,
+alb.Title as album
+FROM tracks as trk
+INNER JOIN albums as alb
+ON alb.AlbumId = trk.AlbumId
+INNER JOIN artists as art
+ON art.ArtistId = alb.ArtistId
+WHERE 
+artista LIKE '%Nação%'
+AND 
+album NOT LIKE 'Da Lama Ao Caos';
--- exercicios/4-refinando-consulta/2-group-by-e-having.sql
+++ exercicios/4-refinando-consulta/2-group-by-e-having.sql
@@ -1,7 +1,36 @@
--- Crie uma consulta usando as tabelas "invoices" e "customers" cujo o resultado seja o total de gastos por cliente
+-- Crie uma consulta usando as tabelas "invoices" e "customers" cujo o resultado seja o Total de gastos por cliente
+SELECT 
+cus.CustomerId as id_cliente,
+cus.FirstName as nome,
+SUM(inv.Total) as Total
+FROM invoices as inv
+LEFT JOIN customers as cus
+ON inv.CustomerId = cus.CustomerId
 
+GROUP BY inv.CustomerId;
 
 -- Na consulta anterior, insira um filtro para que apresente apenas os clientes que gastaram mais de R$30
+SELECT 
+cus.CustomerId as id_cliente,
+cus.FirstName as nome,
+SUM(inv.Total) as Total
+FROM invoices as inv
+LEFT JOIN customers as cus
+ON inv.CustomerId = cus.CustomerId
 
+GROUP BY inv.CustomerId
+HAVING SUM(Total) > 30;
 
 -- Na consulta anterior, insira uma coluna que informe a compra de maior valor feita por cada cliente
+SELECT 
+cus.CustomerId as id_cliente,
+cus.FirstName as nome,
+SUM(inv.Total) as Total,
+MAX(inv.Total) as maior_compra,
+COUNT(inv.Total) as qntd_compra
+FROM invoices as inv
+LEFT JOIN customers as cus
+ON inv.CustomerId = cus.CustomerId
+
+GROUP BY inv.CustomerId
+HAVING SUM(Total) > 30;
